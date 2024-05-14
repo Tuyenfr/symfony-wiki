25bc975d383f3eb395eaee68963acfca5fbdad8f
Tuyenfr <127860619+Tuyenfr@users.noreply.github.com>
2024-05-14 19:34:33 +0200

    maj

--- src/Controller/Admin/VehicleCrudController.php
+++ src/Controller/Admin/VehicleCrudController.php
@@ -7,6 +7,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -28,6 +29,7 @@ class VehicleCrudController extends AbstractCrudController
         yield TextField::new('model', 'modèle');
         yield AssociationField::new('user', 'utilisateur');
         yield TextareaField::new('imageFile')->setFormType(VichImageType::class)->hideOnIndex();
+        yield ImageField::new('imageName')->setBasePath('images/vehicles/')->hideOnForm();
         yield DateField::new('calendar_start_date', 'Début calendrier');
         yield DateField::new('calendar_end_date', 'Fin calendrier');
         
