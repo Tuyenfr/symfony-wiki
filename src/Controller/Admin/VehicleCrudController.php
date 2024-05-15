bce5aafadea12ee47a4228b0179e552b68446c38
Tuyenfr <127860619+Tuyenfr@users.noreply.github.com>
2024-05-15 11:01:16 +0200

    new fields

--- src/Controller/Admin/VehicleCrudController.php
+++ src/Controller/Admin/VehicleCrudController.php
@@ -24,10 +24,17 @@ class VehicleCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
 
-        yield TextField::new('brand', 'marque');
-        yield TextField::new('model', 'modèle');
-        yield TextField::new('numberplate', 'plaque d\'immatriculation');
-        yield AssociationField::new('user', 'utilisateur');
+        yield TextField::new('brand', 'Marque');
+        yield TextField::new('model', 'Modèle');
+        yield TextField::new('numberplate', 'Plaque d\'immatriculation');
+        yield TextField::new('length', 'Longueur');
+        yield TextField::new('height', 'Hauteur');
+        yield TextField::new('gearbox', 'Boîte de vitesse');
+        yield TextField::new('fuel_type', 'Carburant');
+        yield TextField::new('kms', 'Kilométrage');
+        yield TextField::new('adblue', 'Adblue');
+        yield TextEditorField::new('description', 'Description');
+        yield AssociationField::new('user', 'Utilisateur');
         yield TextareaField::new('imageFile')->setFormType(VichImageType::class)->hideOnIndex();
         yield ImageField::new('imageName')->setBasePath('images/vehicles/')->hideOnForm();
         yield DateField::new('calendar_start_date', 'Début calendrier');
