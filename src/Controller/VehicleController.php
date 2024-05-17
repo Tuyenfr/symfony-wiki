3cf86917f83917c475fe4cdd0fc3c27bb3d18bc8
Tuyenfr <127860619+Tuyenfr@users.noreply.github.com>
2024-05-17 09:16:33 +0200

    show vehicle update

--- src/Controller/VehicleController.php
+++ src/Controller/VehicleController.php
@@ -7,6 +7,7 @@ use App\Repository\VehicleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class VehicleController extends AbstractController
 {
@@ -24,9 +25,16 @@ class VehicleController extends AbstractController
     }
 
     #[Route('/vehicle/{id}', name: 'app_vehicle_show')]
-    public function show(Vehicle $vehicle): Response
+    public function show(int $id, EntityManagerInterface $entityManager): Response
     {
+        $vehicle = $entityManager->getRepository(Vehicle::class)->find($id);
+
+        if (!$vehicle) {
+            throw $this->createNotFoundException('No vehicle found for id ' . $id);
+        }
+        
         $websiteName = 'Wikicampers';
+
 
         return $this->render('vehicle/show.html.twig', [
             'vehicle' => $vehicle,
