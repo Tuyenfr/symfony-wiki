3dd11f3c6c5dce8ab96eb5ea39be3f43c72538ff
Tuyenfr <127860619+Tuyenfr@users.noreply.github.com>
2024-05-22 09:56:17 +0200

    French weekday

--- src/Phpscripts/Calendar.php
+++ src/Phpscripts/Calendar.php
@@ -9,6 +9,7 @@ class Calendar
     public function add($date)
     {
         $weekday = date('l', $date);
+        $weekday_fr = null;
         $day = date('d', $date);
         $month = date('M', $date);
         $year = date('Y', $date);
@@ -16,14 +17,41 @@ class Calendar
         $price = 0;
         $availability = 'Y';
 
+        switch ($weekday) {
+        case 'Monday':
+            $weekday_fr = 'Lun';
+            break;
+        case 'Tuesday':
+            $weekday_fr = 'Mar';
+            break;
+        case 'Wednesday':
+            $weekday_fr = 'Mer';
+            break;
+        case 'Thursday':
+            $weekday_fr = 'Jeu';
+            break;
+        case 'Friday':
+            $weekday_fr = 'Ven';
+            break;
+        case 'Saturday':
+            $weekday_fr = 'Sam';
+            break;
+        case 'Sunday':
+            $weekday_fr = 'Dim';
+            break;
+        default:
+            $weekday_fr = "";
+        }
+
         $storedDate = [
             'weekday' => $weekday,
+            'weekday_fr' => $weekday_fr,
             'day' => $day,
             'month' => $month,
             'year' => $year,
             'date' => $fulldate,
             'pricePerDay' => $price,
-            'availability' => $availability
+            'availability' => $availability,
         ];
 
         $this->dates[] = $storedDate;
